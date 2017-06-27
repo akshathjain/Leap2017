f447b5a010c6c126818175d0b05ef4f12d221b7a
akshathjain <akshath.r.jain@gmail.com>
2017-06-27 16:04:55 -0400

    updated the presentations

--- Java Basics/src/Driver.java
+++ Java Basics/src/Driver.java
@@ -4,8 +4,34 @@ Date: 6/27/17
 Purpose: Basic Java Examples
 */
 
+import java.util.Scanner;
+
 public class Driver{
 	public static void main(String[] args){
-		
+		/*Other o = new Other();
+		System.out.println(o.getOtherString());
+
+		System.out.println(Other.getPi());*/
+
+		/*String donkey = new String("Eddie Murphy");
+		String actor = new String("Eddie Murphy");
+		actor += " voiced me";
+
+		System.out.println(actor);
+
+		if(5 == 5){
+			System.out.println("These match!");
+		}
+		else{
+			System.out.println("don't match");
+		}*/
+
+		Scanner scanner = new  Scanner(System.in);
+
+		System.out.print("enter your name: ");
+		String userName = scanner.next();
+		System.out.print("This is your name: " + userName);
+		//scanner.close();
+
 	}
 }
---
+++ Java Basics/src/Other.java
@@ -0,0 +1,19 @@
+public class Other{
+
+	public int someNumber(){
+		return 420;
+	}
+
+	public String getOtherString(){
+		return someString() + " world";
+	}
+
+	private String someString(){
+		return "Hello";
+	}
+
+	public static double getPi(){
+		return 3.14159;
+	}
+
+}
