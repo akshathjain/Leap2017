53d5a53bef38bfffa36865e7f2c8c8c06cb64059
Akshath Jain <akshath.r.jain@gmail.com>
2017-08-02 16:07:49 -0400

    updated lights out

--- Final Projects/Swing Game Example - Lights Out/src/Driver.java
+++ Final Projects/Swing Game Example - Lights Out/src/Driver.java
@@ -9,14 +9,16 @@ import javax.swing.*;
 
 public class Driver{
 	public static void main(String[] args) {
+		//make Java Swing actually look good
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e){
 		}
 
+		//create Jframe
 		JFrame frame = new JFrame("Lights Out");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //so the jframe closes when i press the red x on the top right
+		frame.setSize(500,500); //size of the frame (this is adjustable)
 		Game gamePanel = new Game();
 		frame.add(gamePanel);
 		frame.setVisible(true);
--- Final Projects/Swing Game Example - Lights Out/src/Game.java
+++ Final Projects/Swing Game Example - Lights Out/src/Game.java
@@ -7,11 +7,9 @@ Purpose: Game JPanel for Lights Out
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.*;
 import java.util.*;
-import javax.imageio.*;
-import java.io.*;
 
+@SuppressWarnings("serial") //ignore this (this is just for me)
 public class Game extends JPanel implements MouseListener{
 	private final int NUM_ROWS = 8;
 	private final int NUM_COLUMNS = 8;
@@ -37,33 +35,37 @@ public class Game extends JPanel implements MouseListener{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
+		//get window width and height
 		windowWidth = g.getClipBounds().getWidth();
 		windowHeight = g.getClipBounds().getHeight();
 
 		for(int i = 0; i < NUM_ROWS; i++){
 			for(int j = 0; j < NUM_COLUMNS; j++){
+				
+				//color the screen in a grid like pattern
 				if((i + j) % 2 == 0)
 					g.setColor(new Color(255, 255, 255));
 				else
 					g.setColor(new Color(240,240,240));
 			
-
+				//color the square black if board dictates 
 				if(board[i][j])
 					g.setColor(new Color(0,0,0));
 				
-				g.fillRect((int)Math.round(i * windowWidth / NUM_ROWS), (int)Math.round(j * windowHeight / NUM_COLUMNS), (int)Math.round(windowWidth / NUM_ROWS) + 1, (int)Math.round(windowHeight / NUM_COLUMNS) + 1);
+				//draw the rectangle with the specified color
+				g.fillRect((int)Math.round(j * windowWidth / NUM_COLUMNS), (int)Math.round(i * windowHeight / NUM_ROWS), (int)Math.round(windowWidth / NUM_COLUMNS) + 1, (int)Math.round(windowHeight / NUM_ROWS) + 1);
 
 			}
 		}
 	}
 
-	@Override
+	@Override //called when user clicks the mouse
 	public void mouseClicked(MouseEvent e){
 		this.mouseX = e.getX();
 		this.mouseY = e.getY();
 
 		//change the board state
-		changeState((int)(mouseX / (windowWidth / NUM_ROWS)), (int)(mouseY / (windowWidth / NUM_COLUMNS)));
+		changeState((int)(mouseY / (windowHeight / (double) NUM_ROWS)), (int)(mouseX / (windowWidth / (double) NUM_COLUMNS)));
 
 		//update the screen
 		repaint();
@@ -93,6 +95,7 @@ public class Game extends JPanel implements MouseListener{
 
 	}
 
+	//check if the user has won
 	private boolean checkIfGameFinished(){
 		for(int i = 0; i < NUM_ROWS; i++){
 			for(int j = 0; j < NUM_COLUMNS; j++){
@@ -104,24 +107,20 @@ public class Game extends JPanel implements MouseListener{
 		return true;
 	}
 
-	private int round(double x){
-		return (int) (x + 0.5);
-	}
+	//switches the state of the block clicked and all the blocks around it
+	private void changeState(int row, int col){	
+		board[row][col] = !board[row][col];
 
-	private void changeState(int x, int y){	
-		board[x][y] = !board[x][y];
+		if(row + 1 < NUM_ROWS)
+			board[row + 1][col] = !board[row + 1][col];
 
-		if(x + 1 < NUM_ROWS){
-			board[x + 1][y] = !board[x + 1][y];
-		}
+		if(row - 1 >= 0)
+			board[row - 1][col] = !board[row - 1][col];
 
-		if(x - 1 >= 0)
-			board[x - 1][y] = !board[x - 1][y];
+		if(col + 1 < NUM_COLUMNS)
+			board[row][col + 1] = !board[row][col + 1];
 
-		if(y + 1 < NUM_COLUMNS)
-			board[x][y + 1] = !board[x][y + 1];
-
-		if(y - 1 >= 0)
-			board[x][y - 1] = !board[x][y - 1];
+		if(col - 1 >= 0)
+			board[row][col - 1] = !board[row][col - 1];
 	}
 }
